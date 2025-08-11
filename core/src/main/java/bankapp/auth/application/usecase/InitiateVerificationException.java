2e0476d9b1bfe9c32f0443387ac04b9c28208c77
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-08-11 12:57:19 +0200

    refactor: remove redundant constructor from `InitiateVerificationException`

--- core/src/main/java/bankapp/auth/application/usecase/InitiateVerificationException.java
+++ core/src/main/java/bankapp/auth/application/usecase/InitiateVerificationException.java
@@ -2,10 +2,6 @@ package bankapp.auth.application.usecase;
 
 public class InitiateVerificationException extends RuntimeException{
 
-    public InitiateVerificationException(String msg) {
-        super(msg);
-    }
-
     public InitiateVerificationException(String msg, Throwable e) {
         super(msg, e);
     }
