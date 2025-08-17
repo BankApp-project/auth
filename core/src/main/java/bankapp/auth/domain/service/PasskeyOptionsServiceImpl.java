675dc3f35888bc85b197168a877c7cd97c72fbb0
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-08-17 18:45:06 +0200

    refactor(passkey-options): modularize and optimize credential creation methods
    - Extracted repetitive logic into dedicated private methods for improved readability and maintainability.
    - Modularized `AuthenticatorSelectionCriteria` and `UserEntity` initialization.
    - Introduced helper methods for `challenge` and `hints` generation based on `authMode`.
    - Enhanced structure of `PasskeyOptionsServiceImpl` to align with modular programming practices.

--- core/src/main/java/bankapp/auth/domain/service/PasskeyOptionsServiceImpl.java
+++ core/src/main/java/bankapp/auth/domain/service/PasskeyOptionsServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 public class PasskeyOptionsServiceImpl implements PasskeyOptionsService{
 
     //"smartphone" for smartphone-first userflow
-    //anything else / "default" for default userflow
+    //"default" for default userflow
     //it can be enum later on
     private final String authMode;
     private final String rpId;
@@ -28,53 +28,72 @@ public class PasskeyOptionsServiceImpl implements PasskeyOptionsService{
     }
 
     public PublicKeyCredentialRequestOptions getPasskeyRequestOptions() {
-        byte[] challenge = challengeGenerator.generate();
+        byte[] challenge = getChallenge();
         return new PublicKeyCredentialRequestOptions(challenge, null, null, null, null, null);
     }
 
     public PublicKeyCredentialCreationOptions getPasskeyCreationOptions(User user) {
         String name = user.getEmail().getValue();
+
         UUID userId = user.getId();
+        byte[] userHandle = getUserHandle(userId);
 
-        byte[] challenge = challengeGenerator.generate();
-        byte[] userHandle = ByteArrayUtil.uuidToBytes(userId);
-
-        var userEntity = new PublicKeyCredentialCreationOptions.PublicKeyCredentialUserEntity(userHandle, name, name);
-        var rp = new PublicKeyCredentialCreationOptions.PublicKeyCredentialRpEntity(rpId, rpId);
-
-        var pubKeyCredParamList = getPublicKeyCredentialParametersList();
-
-        /*
-         *    this criteria is to make sure that user will be verificated at new credential registration
-         *    according to: https://www.w3.org/TR/webauthn-3/#dom-authenticatorselectioncriteria-residentkey
-         */
-        String authAttach = "";
-        List<String> hints = new ArrayList<>();
-        if (authMode.equals("smartphone")) {
-            authAttach = "cross-platform";
-            hints.add("hybrid");
-        }
-        var authSelectionCrit = new PublicKeyCredentialCreationOptions.AuthenticatorSelectionCriteria(
-                authAttach,
-                true,
-                "required"
-        );
-
-                return new PublicKeyCredentialCreationOptions(
-                        rp,
-                        userEntity,
-                        challenge,
-                        pubKeyCredParamList,
+        return new PublicKeyCredentialCreationOptions(
+                        getRpEntity(),
+                        getUserEntity(userHandle, name),
+                        getChallenge(),
+                        getPublicKeyCredentialParametersList(),
                         timeout,
                         null,
-                        authSelectionCrit,
-                        hints,
+                        getAuthenticatorSelectionCriteria(),
+                        getHints(),
                         null,
                         null,
                         null
                 );
     }
 
+    private PublicKeyCredentialCreationOptions.PublicKeyCredentialRpEntity getRpEntity() {
+        return new PublicKeyCredentialCreationOptions.PublicKeyCredentialRpEntity(rpId, rpId);
+    }
+
+    private byte[] getUserHandle(UUID userId) {
+        return ByteArrayUtil.uuidToBytes(userId);
+    }
+
+    private List<String> getHints() {
+        List<String> hints = new ArrayList<>();
+        if (authMode.equals("smartphone")) {
+            hints.add("hybrid");
+        }
+        return hints;
+    }
+
+    /**
+     *    this criteria is to make sure that user will be verificated at new credential registration
+     *    according to: <a href="https://www.w3.org/TR/webauthn-3/#dom-authenticatorselectioncriteria-residentkey">W3 Docs</a>
+     **/
+    private PublicKeyCredentialCreationOptions.AuthenticatorSelectionCriteria getAuthenticatorSelectionCriteria() {
+        String authAttach = "";
+        if (authMode.equals("smartphone")) {
+            authAttach = "cross-platform";
+        }
+
+        return new PublicKeyCredentialCreationOptions.AuthenticatorSelectionCriteria(
+                authAttach,
+                true,
+                "required"
+        );
+    }
+
+    private PublicKeyCredentialCreationOptions.PublicKeyCredentialUserEntity getUserEntity(byte[] userHandle, String name) {
+        return new PublicKeyCredentialCreationOptions.PublicKeyCredentialUserEntity(userHandle, name, name);
+    }
+
+
+    private byte[] getChallenge() {
+        return challengeGenerator.generate();
+    }
 
     /**
      * these parameters are default according to official documentation of webauthn:
