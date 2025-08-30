5a24640170893fc3b49bb1549ec4bd5d4abdabbd
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-08-30 18:34:54 +0200

    feat: add application configuration class for Spring context setup

---
+++ infrastructure/src/main/java/bankapp/auth/infrastructure/config/ApplicationConfiguration.java
@@ -0,0 +1,37 @@
+package bankapp.auth.infrastructure.config;
+
+import bankapp.auth.application.shared.UseCase;
+import bankapp.auth.application.shared.port.out.HashingPort;
+import bankapp.auth.application.shared.port.out.persistance.OtpRepository;
+import bankapp.auth.application.verification_initiate.port.out.OtpGenerationPort;
+import bankapp.auth.domain.OtpService;
+import bankapp.auth.domain.port.out.OtpConfigPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@ComponentScan(
+        basePackages = {
+                "bankapp.auth.infrastructure",
+                "bankapp.auth.application"
+        },
+        includeFilters = @ComponentScan.Filter(
+                type = FilterType.ANNOTATION,
+                value = {
+                        UseCase.class,
+                }
+        )
+)
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    public OtpService otpService(
+            OtpGenerationPort otpGenerator,
+            HashingPort hasher,
+            OtpConfigPort config,
+            OtpRepository repository) {
+        return new OtpService(otpGenerator, hasher, config, repository);
+    }
+}
