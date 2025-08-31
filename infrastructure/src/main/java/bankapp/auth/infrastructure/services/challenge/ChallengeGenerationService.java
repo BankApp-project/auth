2f1dfb4e8b069de43e4f346cc769187252e66483
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-08-31 12:00:10 +0200

    feat(ChallengeGenerationServiceTest): implement next test case.

--- infrastructure/src/main/java/bankapp/auth/infrastructure/services/challenge/ChallengeGenerationService.java
+++ infrastructure/src/main/java/bankapp/auth/infrastructure/services/challenge/ChallengeGenerationService.java
@@ -4,13 +4,16 @@ import bankapp.auth.application.shared.port.out.dto.Challenge;
 import bankapp.auth.application.verification_complete.port.out.ChallengeGenerationPort;
 
 import java.time.Clock;
+import java.util.UUID;
 
 public class ChallengeGenerationService implements ChallengeGenerationPort {
     @Override
     public Challenge generate() {
+        var sessionId = UUID.randomUUID();
+
         return new Challenge(
-                null,
-                null,
+                sessionId,
+                new byte[]{123},
                 0L,
                 Clock.systemUTC()
         );
--- infrastructure/src/test/java/bankapp/auth/infrastructure/services/challenge/ChallengeGenerationServiceTest.java
+++ infrastructure/src/test/java/bankapp/auth/infrastructure/services/challenge/ChallengeGenerationServiceTest.java
@@ -3,19 +3,28 @@ package bankapp.auth.infrastructure.services.challenge;
 import bankapp.auth.application.shared.port.out.dto.Challenge;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChallengeGenerationServiceTest {
 
+    ChallengeGenerationService challengeGenerationService = new ChallengeGenerationService();
+
     @Test
     void generate_should_return_challenge() {
-
-        ChallengeGenerationService challengeGenerationService = new ChallengeGenerationService();
 
         var res = challengeGenerationService.generate();
 
         assertNotNull(res);
-        assertInstanceOf(Challenge.class,res);
+        assertInstanceOf(Challenge.class, res);
     }
 
+    @Test
+    void generate_should_return_challenge_with_not_null_values() {
+
+        var res = challengeGenerationService.generate();
+
+        assertThat(res)
+                .hasNoNullFieldsOrProperties();
+    }
 }
