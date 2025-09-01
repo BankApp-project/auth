8a70139567d2ad5503c554b52d505fffb4aabca5
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-09-01 21:42:31 +0200

    feat(testing): upgrade PostgreSQL version and integrate Flyway migrations in test container setup

--- infrastructure/src/test/java/bankapp/auth/infrastructure/WithPostgresContainer.java
+++ infrastructure/src/test/java/bankapp/auth/infrastructure/WithPostgresContainer.java
@@ -1,5 +1,7 @@
 package bankapp.auth.infrastructure;
 
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -10,8 +12,18 @@ public interface WithPostgresContainer {
 
     @Container
     @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:16.6-alpine")
+    PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17-alpine")
             .withDatabaseName("test_auth")
             .withUsername("test")
             .withPassword("test");
+
+    @BeforeEach
+    default void setUp() {
+        Flyway flyway = Flyway.configure().dataSource(
+                postgresContainer.getJdbcUrl(),
+                postgresContainer.getUsername(),
+                postgresContainer.getPassword()
+        ).load();
+        flyway.migrate();
+    }
 }
