5905233f6542708062c2b83eb33cdc0284b696f8
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-09-01 22:01:35 +0200

    refactor(UserRepository): use `@RequiredArgsConstructor` and enforce non-null parameters in methods

--- infrastructure/src/main/java/bankapp/auth/infrastructure/persistance/user/PostgresUserRepository.java
+++ infrastructure/src/main/java/bankapp/auth/infrastructure/persistance/user/PostgresUserRepository.java
@@ -3,21 +3,19 @@ package bankapp.auth.infrastructure.persistance.user;
 import bankapp.auth.application.shared.port.out.persistance.UserRepository;
 import bankapp.auth.domain.model.User;
 import bankapp.auth.domain.model.vo.EmailAddress;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
+@RequiredArgsConstructor
 public class PostgresUserRepository implements UserRepository {
 
     private final UserJpaRepository userJpaRepository;
     private final UserMapper userMapper;
-
-    public PostgresUserRepository(UserJpaRepository userJpaRepository, UserMapper userMapper) {
-        this.userJpaRepository = userJpaRepository;
-        this.userMapper = userMapper;
-    }
 
     @Override
     public Optional<User> findByEmail(EmailAddress email) {
@@ -26,13 +24,13 @@ public class PostgresUserRepository implements UserRepository {
     }
 
     @Override
-    public void save(User user) {
+    public void save(@NonNull User user) {
         JpaUser jpaUser = userMapper.toJpaUser(user);
         userJpaRepository.save(jpaUser);
     }
 
     @Override
-    public Optional<User> findById(UUID userId) {
+    public Optional<User> findById(@NonNull UUID userId) {
         return userJpaRepository.findById(userId)
             .map(userMapper::toDomainUser);
     }
