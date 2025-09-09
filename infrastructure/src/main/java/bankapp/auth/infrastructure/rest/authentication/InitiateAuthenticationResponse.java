ec880ba382e1b9a7e06437b41d8f5d2877ad7342
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-09-09 17:04:32 +0200

    feat(auth): add integration test for initiate authentication flow
    - Introduced `InitiateAuthenticationIT` to validate the authentication initiation endpoint.
    - Renamed properties in `InitiateAuthenticationResponse` for better clarity.
    
    closes #53

--- infrastructure/src/main/java/bankapp/auth/infrastructure/rest/authentication/InitiateAuthenticationResponse.java
+++ infrastructure/src/main/java/bankapp/auth/infrastructure/rest/authentication/InitiateAuthenticationResponse.java
@@ -3,6 +3,6 @@ package bankapp.auth.infrastructure.rest.authentication;
 import bankapp.auth.application.shared.port.out.dto.PublicKeyCredentialRequestOptions;
 
 public record InitiateAuthenticationResponse(
-        PublicKeyCredentialRequestOptions options,
-        String ChallengeId) {
+        PublicKeyCredentialRequestOptions loginOptions,
+        String challengeId) {
 }
---
+++ infrastructure/src/test/java/bankapp/auth/infrastructure/usecases/it/InitiateAuthenticationIT.java
@@ -0,0 +1,67 @@
+package bankapp.auth.infrastructure.usecases.it;
+
+
+import bankapp.auth.application.shared.port.out.persistance.ChallengeRepository;
+import bankapp.auth.infrastructure.WithRedisContainer;
+import bankapp.auth.infrastructure.rest.authentication.InitiateAuthenticationResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional // Automatically rolls back database changes after each test
+public class InitiateAuthenticationIT implements WithRedisContainer {
+
+    private static final String INITIATE_AUTHENTICATION_ENDPOINT = "/authentication/initiate";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ChallengeRepository challengeRepository;
+
+    @Test
+    void should_return_loginResponse() throws Exception {
+        var res = mockMvc.perform(get(INITIATE_AUTHENTICATION_ENDPOINT))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.challengeId").isNotEmpty())
+                .andExpect(jsonPath("$.loginOptions.challenge").isNotEmpty())
+                .andExpect(jsonPath("$.loginOptions.timeout").isNumber())
+                .andExpect(jsonPath("$.loginOptions.rpId").isString())
+                .andReturn();
+
+        assertChallengePersisted(res);
+    }
+
+    private void assertChallengePersisted(MvcResult res) throws UnsupportedEncodingException, JsonProcessingException {
+        var jsonBody = res.getResponse().getContentAsString();
+        InitiateAuthenticationResponse responseObj = objectMapper.readValue(jsonBody,InitiateAuthenticationResponse.class);
+
+        var challengeId = responseObj.challengeId();
+        var challengeUUID = UUID.fromString(challengeId);
+        var loadedChallenge = challengeRepository.load(challengeUUID);
+
+        assertThat(loadedChallenge).isPresent();
+    }
+}
