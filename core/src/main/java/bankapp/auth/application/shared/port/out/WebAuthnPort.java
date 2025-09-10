a268a8142551de9260614b0f21185c8a7e242690
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-09-10 09:58:19 +0200

    refactor: change parameters from WebAuthnPort from `Challenge sessionData` to `Challenge challengeData`

--- core/src/main/java/bankapp/auth/application/shared/port/out/WebAuthnPort.java
+++ core/src/main/java/bankapp/auth/application/shared/port/out/WebAuthnPort.java
@@ -6,12 +6,12 @@ import bankapp.auth.domain.model.Passkey;
 
 public interface WebAuthnPort {
 
-    PasskeyRegistrationData confirmRegistrationChallenge(String registrationResponseJSON, Challenge sessionData);
+    PasskeyRegistrationData confirmRegistrationChallenge(String registrationResponseJSON, Challenge challengeData);
 
     /**
      *
      * @return updated `Passkey` with `signCount`++
      */
     //todo I need to know what fields from Passkey is used for this method and fetch only those. using dto record or interface with JPA repo
-    Passkey confirmAuthenticationChallenge(String authenticationResponseJSON, Challenge sessionData, Passkey passkey);
+    Passkey confirmAuthenticationChallenge(String authenticationResponseJSON, Challenge challengeData, Passkey passkey);
 }
