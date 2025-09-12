c87cec88ac8a068ef85971b986ed3f7daf2c3ba0
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-09-12 18:53:30 +0200

    fix: refactor objectConverter bean to use new Mappers for bean creation.
    - it was causing problems as WebAuthn modifies ObjectMapper instance.

--- infrastructure/src/main/java/bankapp/auth/infrastructure/crosscutting/config/WebAuthn4jConfig.java
+++ infrastructure/src/main/java/bankapp/auth/infrastructure/crosscutting/config/WebAuthn4jConfig.java
@@ -1,27 +1,18 @@
 package bankapp.auth.infrastructure.crosscutting.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.cbor.databind.CBORMapper;
 import com.webauthn4j.converter.util.ObjectConverter;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebAuthn4jConfig {
 
-    private final ObjectMapper objectMapper;
-    private final ObjectMapper cborMapper;
-
-    public WebAuthn4jConfig(
-            @Qualifier("objectMapper") ObjectMapper objectMapper,
-            @Qualifier("cborMapper") ObjectMapper cborMapper
-    ) {
-        this.objectMapper = objectMapper;
-        this.cborMapper = cborMapper;
-    }
-
     @Bean
     public ObjectConverter objectConverter() {
-        return new ObjectConverter(objectMapper, cborMapper);
+        return new ObjectConverter(
+                new ObjectMapper(),
+                new CBORMapper());
     }
 }
