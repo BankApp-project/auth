1337f9c7800cd77f52d6ac9b6d221feeaa010d45
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-09-19 13:08:38 +0200

    feat(security): add SecurityFilterChain permissive configuration and remove RSAProperties tests

--- infrastructure/src/main/java/bankapp/auth/infrastructure/crosscutting/config/SecurityConfiguration.java
+++ infrastructure/src/main/java/bankapp/auth/infrastructure/crosscutting/config/SecurityConfiguration.java
@@ -8,8 +8,11 @@ import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.SecureRandom;
 import java.security.interfaces.RSAPrivateKey;
@@ -39,5 +42,15 @@ public class SecurityConfiguration {
         JWKSet jwkSet = new JWKSet(rsaKey);
 
         return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Often disabled for stateless APIs
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                );
+        return http.build();
     }
 }
