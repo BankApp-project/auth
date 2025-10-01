41cd11dad66e30cd9bfc7e964a7a1072ca6c078d
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-10-01 11:50:55 +0200

    feat(infra): enable AmqpRunner for exchange declaration in non-test profiles

--- infrastructure/src/main/java/bankapp/auth/infrastructure/crosscutting/config/AmqpRunner.java
+++ infrastructure/src/main/java/bankapp/auth/infrastructure/crosscutting/config/AmqpRunner.java
@@ -1,15 +1,22 @@
 package bankapp.auth.infrastructure.crosscutting.config;
 
-//@Profile("!test & !test-postgres")
-//@Component
-//@RequiredArgsConstructor
-//public class AmqpRunner implements CommandLineRunner {
-//
-//    private final AmqpAdmin amqpAdmin;
-//    private final TopicExchange notificationTopicExchange;
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        amqpAdmin.declareExchange(notificationTopicExchange);
-//    }
-//}
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("!test & !test-postgres")
+@Component
+@RequiredArgsConstructor
+public class AmqpRunner implements CommandLineRunner {
+
+    private final AmqpAdmin amqpAdmin;
+    private final TopicExchange notificationTopicExchange;
+
+    @Override
+    public void run(String... args) {
+        amqpAdmin.declareExchange(notificationTopicExchange);
+    }
+}
