ac90610763d58afd0a56330d27d2ec996918e7f8
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-10-02 10:28:37 +0200

    refactor(auth): remove unused `EventPublisherPort` and `EventTemplate`
    - Deleted `EventPublisherPort` interface and `EventTemplate` class as they are not in use.
    - Simplified codebase by eliminating unused components.

--- core/src/main/java/bankapp/auth/application/shared/port/out/service/EventPublisherPort.java
+++
@@ -1,7 +0,0 @@
-package bankapp.auth.application.shared.port.out.service;
-
-import bankapp.auth.application.shared.EventTemplate;
-
-public interface EventPublisherPort {
-    void publish(EventTemplate object);
-}
