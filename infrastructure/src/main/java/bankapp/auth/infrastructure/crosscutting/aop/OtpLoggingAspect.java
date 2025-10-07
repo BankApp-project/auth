e3b9bfa8113680e938c61dc692af3e143c239831
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-10-07 15:45:47 +0200

    feat(config): add support for conditional OTP console logging
    - Introduced `APP_OTP_CONSOLE_ENABLED` to toggle OTP logging to the console.
    - Updated `application.yaml`, `.env.example`, `.env.docker`, and `.env.docker.example` to include the new property.
    - Enhanced `OtpLoggingAspect` with environment-based conditional logging, excluding production environments.

--- infrastructure/src/main/java/bankapp/auth/infrastructure/crosscutting/aop/OtpLoggingAspect.java
+++ infrastructure/src/main/java/bankapp/auth/infrastructure/crosscutting/aop/OtpLoggingAspect.java
@@ -4,17 +4,23 @@ import bankapp.auth.domain.model.vo.EmailAddress;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
 @Component
+@ConditionalOnBooleanProperty(name = "app.config.otp.console-enabled")
+@Profile("!prod")
 public class OtpLoggingAspect {
 
     @Before(value = "execution(* bankapp.auth.infrastructure.driven.notification.NotificationAdapter.sendOtpToUserEmail(..)) && args(userEmail, otpValue)", argNames = "userEmail,otpValue")
     public void logOtpToConsole(EmailAddress userEmail, String otpValue) {
         log.info("=================================================");
         log.info("OTP Code for {}: {}", userEmail.getValue(), otpValue);
+        log.warn("=================================================");
+        log.warn("This log is intended for development purposes only.");
         log.info("=================================================");
     }
 }
