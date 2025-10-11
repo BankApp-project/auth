b189b2d02d38ab9d246b34f0ffaa9bb0540f3354
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-10-11 11:36:22 +0200

    refactor(db): migrate `id` column in Passkey table to `id_bytes` as primary key

--- infrastructure/src/main/resources/db/migration/V2__.sql
+++ infrastructure/src/main/resources/db/migration/V2__.sql
@@ -2,5 +2,16 @@ ALTER TABLE passkey
     ADD id_bytes BYTEA;
 
 UPDATE passkey
-SET id_bytes = id::bytea
-WHERE id_bytes IS NULL;
+SET id_bytes = uuid_send(id);
+
+ALTER TABLE passkey
+    DROP COLUMN id;
+
+ALTER TABLE passkey
+    RENAME id_bytes TO id;
+
+ALTER TABLE passkey
+    ALTER COLUMN id SET NOT NULL;
+
+ALTER TABLE passkey
+    ADD PRIMARY KEY (id);
